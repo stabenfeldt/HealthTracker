1e08e7e34d9593af0f4fb3cb9e3b5352ec4a776e
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-03 10:02:00 +0200

    Added wallaby

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule HealthTracker.MixProject do
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"}
+      {:telemetry_poller, "~> 1.0"},
+      {:wallaby, "~> 0.30.5", [runtime: false, only: :test]}
     ]
   end
 
