167d578affd1101fb5d05dba54f069848946d3e0
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-03 13:33:13 +0200

    added sign_in

--- test/health_tracker_web/controllers/page_controller_test.exs
+++ test/health_tracker_web/controllers/page_controller_test.exs
@@ -1,5 +1,15 @@
 defmodule HealthTrackerWeb.PageControllerTest do
   use HealthTracker.FeatureCase, async: true
+  defp sign_in(session, as: user) do
+    session
+    |> visit("/")
+    |> click(Query.link("Log in"))
+    |> fill_in(Query.text_field("Email"), with: user.email)
+    |> fill_in(Query.text_field("Password"), with: user.password)
+    |> click(Query.button("Sign in"))
+    |> take_screenshot(name: "sign_in")
+    |> assert_has(Query.css(".current_user", text: "user@example.com"))
+  end
 
   test "user can visit homepage", %{session: session} do
     session
