24ccecc4919d14854737ea82c1e135ea8e9fa464
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-09 09:41:12 +0200

    Revert "Keep it DRY"
    This reverts commit 8606c4f7c18c9fa6e9d0ac53e0914113d6840deb.

--- test/health_tracker_web/controllers/page_controller_test.exs
+++ test/health_tracker_web/controllers/page_controller_test.exs
@@ -3,15 +3,7 @@ defmodule HealthTrackerWeb.PageControllerTest do
   import Wallaby.Browser
   alias HealthTracker.Factory
 
-  # Setup a user account once before running any tests
-  setup_all do
-    user = Factory.insert(:user)
-    {:ok, %{user: user}}
-  end
   defp sign_in(session, as: user) do
-    IO.puts("Signing in as #{user.email}")
-    IO.puts("Signing in with password #{user.password}")
-
     session
     |> visit("/")
     |> click(Query.link("Log in"))
@@ -19,7 +11,7 @@ defmodule HealthTrackerWeb.PageControllerTest do
     |> fill_in(Query.text_field("Password"), with: user.password)
     |> click(Query.button("Sign in"))
     |> take_screenshot(name: "sign_in")
-    |> assert_has(Query.css(".current_user", text: user.email))
+    |> assert_has(Query.css(".current_user", text: "user@example.com"))
   end
 
   test "user can visit homepage", %{session: session} do
@@ -30,7 +22,7 @@ defmodule HealthTrackerWeb.PageControllerTest do
 
   # Test a protected page
   test "GET /weights", %{session: session} do
-    user = Factory.insert(:user)
+    user = Factory.insert(:user, email: "user@example.com")
 
     session
     |> sign_in(as: user)
