4d310b3caa3838a40a90f3e41f218071d8773328
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-10 13:58:27 +0200

    Remove ExMachina from dev

--- mix.exs
+++ mix.exs
@@ -26,9 +26,6 @@ defmodule HealthTracker.MixProject do
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
 
-  # Allow ExMachina to be compiled in dev
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
-
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
