6516a6a768a8afa9d9a076860c687dfe06e0ef15
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-10 11:35:03 +0200

    Revert changes

--- test/support/feature_case.ex
+++ test/support/feature_case.ex
@@ -1,9 +1,6 @@
 defmodule HealthTracker.FeatureCase do
   use ExUnit.CaseTemplate
 
-  use HealthTrackerWeb.ConnCase
-  # use HealthTracker.DataCase
-
   using do
     quote do
       use Wallaby.DSL
@@ -14,14 +11,11 @@ defmodule HealthTracker.FeatureCase do
   end
 
   setup tags do
-    #  pid = Ecto.Adapters.SQL.Sandbox.start_owner!(HealthTracker.Repo, shared: not tags[:async])
-    #  on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(HealthTracker.Repo)
 
-    #  # :ok = Ecto.Adapters.SQL.Sandbox.checkout(HealthTracker.Repo)
-
-    #  # unless tags[:async] do
-    #  #  Ecto.Adapters.SQL.Sandbox.mode(HealthTracker.Repo, {:shared, self()})
-    #  # end
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(HealthTracker.Repo, {:shared, self()})
+    end
 
     metadata = Phoenix.Ecto.SQL.Sandbox.metadata_for(HealthTracker.Repo, self())
     {:ok, session} = Wallaby.start_session(metadata: metadata)
