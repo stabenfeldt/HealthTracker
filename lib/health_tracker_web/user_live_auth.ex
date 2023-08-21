0c59f1b13f933b93685976e2f804970014edb933
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-21 09:19:37 +0200

    cleanup

--- lib/health_tracker_web/user_live_auth.ex
+++ lib/health_tracker_web/user_live_auth.ex
@@ -1,12 +1,9 @@
 defmodule HealthTrackerWeb.UserLiveAuth do
   import Phoenix.Component
   import Phoenix.LiveView
-  # from `mix phx.gen.auth`
   alias HealthTracker.Accounts
 
   def on_mount(:default, _params, %{"user_token" => user_token} = _session, socket) do
-    #IO.puts("on_mount")
-
     socket =
       assign_new(socket, :current_user, fn ->
         Accounts.get_user_by_session_token(user_token)
