d620e2223dda16be9a3fbea7c61c5a08ba34b7b5
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-08-26 07:08:24 +0200

    user_id is a part of the params

--- test/health_tracker/health_stats_test.exs
+++ test/health_tracker/health_stats_test.exs
@@ -33,13 +33,13 @@ defmodule HealthTracker.HealthStatsTest do
       user = Factory.insert(:user)
       valid_attrs = %{weight: 120.5, user_id: user.id}
 
-      assert {:ok, %Weight{} = weight} = HealthStats.create_weight(user.id, valid_attrs)
+      assert {:ok, %Weight{} = weight} = HealthStats.create_weight(valid_attrs)
       assert weight.weight == 120.5
     end
 
     test "create_weight/1 with invalid data returns error changeset" do
       user = Factory.insert(:user)
-      assert {:error, %Ecto.Changeset{}} = HealthStats.create_weight(user.id, @invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = HealthStats.create_weight(@invalid_attrs)
     end
 
     test "update_weight/2 with valid data updates the weight" do
