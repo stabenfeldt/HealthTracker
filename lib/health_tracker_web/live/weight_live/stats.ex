a543bde58716d314808f152252b65a6213f80e55
Martin Stabenfeldt <martin@stabenfeldt.net>
2023-09-15 12:05:23 +0200

    use dates for the last 4 weeks on the X axis.

--- lib/health_tracker_web/live/weight_live/stats.ex
+++ lib/health_tracker_web/live/weight_live/stats.ex
@@ -1,10 +1,30 @@
 defmodule HealthTrackerWeb.WeightLive.Stats do
   use HealthTrackerWeb, :live_view
+  alias HealthTracker.DateUtils
 
   on_mount HealthTrackerWeb.UserLiveAuth
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    # Labels goes on the X-graph.
+    # This will be the last 4 weeks.
+    labels = get_timeline()
+
+    # Values goes on the Y-graph.
+    # Enum.map(labels, fn _ -> get_timeline(:weeks) end)
+    values = get_weight_values_for_user(current_user(socket))
+
+    {:ok,
+     assign(socket,
+       chart_data: %{
+         labels: labels,
+         values: values
+       }
+     )}
+  end
+
+  # The last 4 weeks
+  def get_timeline() do
+    DateUtils.last_four_weeks_dates()
   end
 
   def render(assigns) do
